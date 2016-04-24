b8ef256f82d63cb637b83c5ffdcf4fc41e3fb363
linqinghan <linqinghan@gmail.com>
2016-04-24 15:36:49 +0800

    Update listdir.py

--- listdir.py
+++ listdir.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 
-#遍历目录，目录通过运行参数输入
+#遍历目录，目录名通过参数输入
 
 from sys import argv
 
@@ -10,8 +10,10 @@ script, dir = argv
 
 if os.path.exists(dir) == False:
 	print "Sorry The Dir[%s] is Not Exist!" %dir
+elif os.path.isdir(dir) == False:
+	print "%s Is Not A Directory!" %dir
 else:
-	print "Start List File In Directory[%s] :" %dir
-	
+	print "Start List File In Directory[%s] :" %dir	
 	for filename in os.listdir(dir):
 		print filename
+
